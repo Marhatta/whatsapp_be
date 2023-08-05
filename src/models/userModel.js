b28481e212fbc64babe122f0361eeb6e817756da
Marhatta <vishal.marhatta@gmail.com>
2023-08-05 19:58:39 +0530

    added user model

---
+++ src/models/userModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide your name"]
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide your email address"],
+      unique: [true, "This email address already exists"],
+      lowercase: true,
+      validate: [validator.isEmail, "Please provide a valid email address"]
+    },
+    picture: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dkd5jblv5/image/upload/v1675976806/Default_ProfilePicture_gjngnb.png"
+    },
+    status: {
+      type: String,
+      default: "Hey there! I am using Whatsapp"
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide your password"],
+      minLength: [
+        6,
+        "Please make sure your password is atleast 6 characters long"
+      ],
+      maxLength: [
+        128,
+        "Please make sure your password is less than 128 characters long"
+      ]
+    }
+  },
+  { collection: "users", timestamps: true }
+);
+
+const UserModel =
+  mongoose.models.UserModel || mongoose.Model("UserModel", userSchema);
+
+export default UserModel;
